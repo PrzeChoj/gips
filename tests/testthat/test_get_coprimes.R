87e15a92becf2dec08de0f79390b237aa31a1b25
PrzeChoj <premysl.choj@gmail.com>
2022-03-24 17:27:29 +0100

    Additional test

--- tests/testthat/test_get_coprimes.R
+++ tests/testthat/test_get_coprimes.R
@@ -1,3 +1,4 @@
-test_that('get_coprimes example',{
+test_that('get_coprimes examples',{
     expect_equal(get_coprimes(10), c(1,3,7,9))
+    expect_equal(length(get_coprimes(100)), numbers::eulersPhi(100))
     })
--- tests/testthat/test_get_representative.R
+++ tests/testthat/test_get_representative.R
@@ -3,9 +3,12 @@ suppressMessages(library(permutations))
 p1 <- as.word(c(2,1,3,6,4,5))
 expected_p <- as.word(c(2,1,3,5,6,4))
 
-test_that('get_representative example',{
+test_that('get_representative examples',{
     expect_equal(cycle2word(get_representative(p1)),
                  expected_p)
+  
+    expect_equal(get_representative(as.word(numeric(0))),
+                 as.word(numeric(0)))
 })
 
 
