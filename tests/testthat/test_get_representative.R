d2f89bc513fbd34cd46a08c006ada15a4c0b77fe
PrzeChoj <premysl.choj@gmail.com>
2022-03-25 19:06:18 +0100

    test_get_representative changed

--- tests/testthat/test_get_representative.R
+++ tests/testthat/test_get_representative.R
@@ -1,14 +1,12 @@
-suppressMessages(library(permutations))
-
-p1 <- as.word(c(2,1,3,6,4,5))
-expected_p <- as.word(c(2,1,3,5,6,4))
+p1 <- permutations::as.word(c(2,1,3,6,4,5))
+expected_p <- permutations::as.word(c(2,1,3,5,6,4))
 
 test_that('get_representative examples',{
-    expect_equal(cycle2word(get_representative(p1)),
+    expect_equal(permutations::cycle2word(get_representative(p1)),
                  expected_p)
   
-    expect_equal(get_representative(as.word(numeric(0))),
-                 as.word(numeric(0)))
+    expect_equal(get_representative(permutations::as.word(numeric(0))),
+                 permutations::as.cycle(permutations::as.word(numeric(0))))
 })
 
 
