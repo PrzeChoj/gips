845476f5c36cacd482b3a3c2cdd789d87a36bfd4
MrDomani <seriousmorgen@protonmail.com>
2022-03-31 13:24:50 +0200

    added new test

--- tests/testthat/test_prepare_orthogonal_matrix.R
+++ tests/testthat/test_prepare_orthogonal_matrix.R
@@ -1,5 +1,27 @@
 source('setup_example.R')
 
+matrix_invariant_by_example_perm <- matrix(c(
+    2, 1, 1, 3, 3, 4,
+    1, 2, 1, 3, 3, 4,
+    1, 1, 2, 3, 3, 4,
+    3, 3, 3, 5, 6, 7,
+    3, 3, 3, 6, 5, 7,
+    4, 4, 4, 7, 7, 8
+), byrow=TRUE, ncol=6)
+
+test_that('orthogonal matrix fulfills condition from example 6',{
+    U_Gamma <- prepare_orthogonal_matrix(example_perm, 6, example_basis)
+    tested_matrix <- t(U_Gamma) %*% matrix_invariant_by_example_perm %*% U_Gamma
+    tested_elements <- tested_matrix
+    tested_elements[1:3, 1:3] <- 0
+    tested_elements[4,4] <- 0
+    tested_elements[5,5] <- 0
+    tested_elements[6,6] <- 0
+    expect_equal(tested_elements,
+                 matrix(rep(0, 6*6), ncol=6))
+    expect_true(isSymmetric(tested_matrix[1:3, 1:3]))
+})
+
 test_that('prepare_orthogonal_matrix works for example', {
     expect_equal(
         prepare_orthogonal_matrix(example_perm, 6, example_basis),
