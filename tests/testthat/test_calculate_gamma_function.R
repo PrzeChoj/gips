fd51f97574a7198b1cd771d4fc74107a0523148a
MrDomani <seriousmorgen@protonmail.com>
2022-05-18 10:08:09 +0200

    G_function and phi_part now dont calculate product

--- tests/testthat/test_calculate_gamma_function.R
+++ tests/testthat/test_calculate_gamma_function.R
@@ -5,14 +5,14 @@ cprim <- permutations::id
 test_that('calculate_gamma_omega returns proper value',{
   # perm = (1)(2)
   structure_constants <- get_structure_constants(cprim, 2)
-  
+
   dim_omega_i <- structure_constants[["dim_omega"]][1] # 3
   r_i <- structure_constants[["r"]][1] # 2
   d_i <- structure_constants[["d"]][1] # 1
   k_i <- structure_constants[["k"]][1] # 1
-  
+
   lambda <- 1/2 * k_i * (delta-2) + dim_omega_i/r_i # 2
-  
+
   expect_equal(calculate_gamma_omega(lambda=lambda,
                                      dim_omega_i=dim_omega_i,
                                      r_i=r_i,
@@ -21,14 +21,14 @@ test_that('calculate_gamma_omega returns proper value',{
 
   # perm = (1,2)
   structure_constants <- get_structure_constants(c, 2)
-  
+
   dim_omega_i <- structure_constants[["dim_omega"]][1] # 1
   r_i <- structure_constants[["r"]][1] # 1
   d_i <- structure_constants[["d"]][1] # 1
   k_i <- structure_constants[["k"]][1] # 1
-  
+
   lambda <- 1/2 * k_i * (delta-2) + dim_omega_i/r_i # 2
-  
+
   expect_equal(calculate_gamma_omega(lambda=lambda,
                                      dim_omega_i=dim_omega_i,
                                      r_i=r_i,
@@ -40,7 +40,7 @@ test_that('calculate_gamma_omega returns proper value',{
 test_that("when L is 1, G_function returns the same value as calculate_gamma_omega", {
   structure_constants <- get_structure_constants(cprim, 2)
   lambda <- 1/2 * structure_constants[['k']][1] * (delta-2) + structure_constants[['dim_omega']][1]/structure_constants[['r']][1]
-  
+
   expect_equal(G_function(cprim, structure_constants, delta),
                calculate_gamma_omega(lambda,
                                      structure_constants[['dim_omega']][1],
@@ -52,17 +52,17 @@ test_that("G_function example from documentation", {
   perm_size <- 6
   perm <- permutations::as.cycle(permutations::as.word(c(2,3,1,5,4,6)))
   structure_constants <- get_structure_constants(perm, perm_size)
-  
-  expect_true(abs(G_function(perm, structure_constants, 3) - 16.443561) < 0.0001)
+
+  expect_true(abs(prod(G_function(perm, structure_constants, 3)) - 16.443561) < 0.0001)
 })
 
 test_that("calculate_gamma_function has desired properties", {
   expect_gt(calculate_gamma_function(permutations::id, 2, 0.50000001),
             calculate_gamma_function(permutations::id, 2, 0.5001))
-  
+
   expect_gt(calculate_gamma_function(permutations::id, 2, 0.500000000001),
             calculate_gamma_function(permutations::id, 2, 0.50000001))
-  
+
   expect_warning(calculate_gamma_function(permutations::id, 2, 0.5))
 })
 
