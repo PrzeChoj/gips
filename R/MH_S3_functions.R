35d1048400753c31638274cd922fae5b993746ae
PrzeChoj <premysl.choj@gmail.com>
2022-05-19 21:05:28 +0200

    MH.plot has both plots

--- R/MH_S3_functions.R
+++ R/MH_S3_functions.R
@@ -21,48 +21,60 @@ print.MH <- function(x, ...){
 #' Plot Metropolis-Hasting convergence
 #' 
 #' Plot method for MH objects returned by \code{\link{MH}}.
-#' @param x object of class MH. Output of MH function.
+#' @param x Object of class MH. Output of MH function.
 #' @param logarithmic boolean.
 #' @param type To plot the maximum found value or all found values.
 #' @param title_text Text to be in a title of the plot.
 #' @param xlabel Text to be on the bottom of the plot.
 #' @param ylabel Text to be on the left of the plot.
+#' @param show_legend boolean.
 #' @param ... additional arguments passed to \code{\link{print}}.
 #' 
 #' @return Invisible NULL.
 #' @export
-plot.MH <- function(x, logarithmic=TRUE, type="max",
+plot.MH <- function(x, logarithmic=TRUE,
                     title_text="Convergence plot",
                     xlabel="number of function calls",
-                    ylabel=NULL, ...){
+                    ylabel=NULL, show_legend=TRUE,
+                    ...){
   stopifnot("MH" %in% class(x))
   if(is.null(ylabel)){
     ylabel <- ifelse(logarithmic,
-                     "biggest log of a function",
-                     "biggest value of a function")
+                     "log of a function",
+                     "value of a function")
   }
   if(logarithmic){  # values of goal function are logarithmic by default
     y_values_from <- x[["goal_function_logvalues"]]
   }else{
     y_values_from <- exp(x[["goal_function_logvalues"]])
   }
-  if(type == "max"){
-    y_values <- cummax(y_values_from)
-  }else{
-    y_values <- y_values_from
-  }
+
+  y_values_max <- cummax(y_values_from)
+  y_values_all <- y_values_from
   
-  xlim <- c(1, length(x[["goal_function_logvalues"]]))
-  ylim <- range(y_values)
+  num_of_steps <- length(y_values_max)
+  
+  xlim <- c(1, num_of_steps)
+  ylim <- c(y_values_max[1], y_values_max[num_of_steps])
   
   graphics::plot.new()
   graphics::plot.window(xlim, ylim)
-  graphics::lines.default(1:length(y_values), y_values, ...)
+  graphics::lines.default(1:num_of_steps, y_values_all, type = "l",
+                          col = "blue", ...)
+  graphics::lines.default(1:num_of_steps, y_values_max, lwd=3,
+                          lty = 2, col = "red", ...)
   
   graphics::title(main = title_text, xlab = xlabel, ylab = ylabel, ...)
   graphics::axis(1, ...)
   graphics::axis(2, ...)
   graphics::box(...)
+  
+  if(show_legend)
+    legend("bottomright", inset=.02,
+           legend = c("All calculated function values",
+                      "Maximum function values calculated"),
+           col = c("blue", "red"), lty = 1:2, cex = 0.7, box.lty=0,
+           lwd = c(1, 3))
   
   invisible(NULL)
 }
